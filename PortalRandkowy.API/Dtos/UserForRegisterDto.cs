7eca5a4fdc27ea533a20986587c330147c72980d
ksarama <ksarama@andea.com>
2020-04-08 20:40:18 +0200

    21. Walidacja

--- PortalRandkowy.API/Dtos/UserForRegisterDto.cs
+++ PortalRandkowy.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PortalRandkowy.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required(ErrorMessage = "Nazwa użytkownika jest wymagana")]
         public string Username { get; set; }
+        [Required(ErrorMessage = "Hasło jest wymagane")]
+        [MaxLength(12, ErrorMessage = "Hasło może mieć maksymalnie 12 znaków")]
+        [MinLength(6, ErrorMessage = "Hasło powinno mieć conajmniej 6 znaków")]
         public string Password { get; set; }
     }
 }
