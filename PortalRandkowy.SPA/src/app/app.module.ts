ff1e74db63401df75941207f03e94cfa21f9ea33
ksarama <ksarama@andea.com>
2020-04-14 10:59:47 +0200

    25. Formularz logowania

--- PortalRandkowy.SPA/src/app/app.module.ts
+++ PortalRandkowy.SPA/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
@@ -14,6 +15,7 @@ import { NavComponent } from './nav/nav.component';
    ],
    imports: [
       BrowserModule,
+      FormsModule,
       HttpClientModule
    ],
    providers: [],
