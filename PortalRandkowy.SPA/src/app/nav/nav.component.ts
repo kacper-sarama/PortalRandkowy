43cab085e82ee7b0a131177fb7ef8113a000fa7e
ksarama <ksarama@andea.com>
2020-04-14 12:49:32 +0200

    27. Użycie serwisu w komponencie.

--- PortalRandkowy.SPA/src/app/nav/nav.component.ts
+++ PortalRandkowy.SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,13 +10,18 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   login() {
-    console.log(this.model);
+    this.authService.login(this.model)
+      .subscribe(next => {
+        console.log("Zalogowałeś się do aplikacji");
+      }, error => {
+        console.log("Wystąpił błąd logowania");
+      });
   }
 
 }
