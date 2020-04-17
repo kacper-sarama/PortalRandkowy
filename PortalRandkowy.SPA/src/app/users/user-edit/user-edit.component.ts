022268f85a3933052befa78f210ffd832b069913
ksarama <ksarama@andea.com>
2020-04-17 12:24:30 +0200

    68. Edycja użytkownika, przygotowanie szablonu.

--- PortalRandkowy.SPA/src/app/users/user-edit/user-edit.component.ts
+++ PortalRandkowy.SPA/src/app/users/user-edit/user-edit.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/_models/user';
 import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryOptions } from '@kolkov/ngx-gallery';
+import { NgxGalleryImage } from '@kolkov/ngx-gallery';
+import { NgxGalleryAnimation } from '@kolkov/ngx-gallery';
 
 @Component({
   selector: 'app-user-edit',
@@ -10,6 +13,8 @@ import { ActivatedRoute } from '@angular/router';
 export class UserEditComponent implements OnInit {
 
   user: User;
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
@@ -17,5 +22,33 @@ export class UserEditComponent implements OnInit {
     this.activatedRoute.data.subscribe(data => {
       this.user = data.user;
     });
+
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        thumbnailsColumns: 4,
+        imagePercent: 100,
+        preview: false,
+        imageAnimation: NgxGalleryAnimation.Slide
+
+      }
+    ];
+
+    this.galleryImages = this.getImages();
+  }
+
+  getImages() {
+    const imagesUrls = [];
+
+    for (let i = 0; i < this.user.photos.length; i++) {
+      imagesUrls.push({
+        small: this.user.photos[i].url,
+        medium: this.user.photos[i].url,
+        big: this.user.photos[i].url,
+        description: this.user.photos[i].description
+      });
+    }
+    return imagesUrls;
   }
 }
